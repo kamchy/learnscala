7b6744d90ff575ba445dd27225cd4fe9266c6821
Kamila Chyla <kamila@chyla.pl>
2011-11-05 22:26:52 +0100

    Added threads that modify map.

--- simple.scala
+++ simple.scala
@@ -1,43 +1,117 @@
 import scala.swing._
-import java.awt.Dimension
 import event._
-import scala.collection.mutable.LinkedList
+import scala.collection.immutable.TreeMap
+import java.awt.Dimension
+import java.lang.Thread
+import scala.util.Random
 
-object App extends SimpleSwingApplication {
-  var strList = LinkedList[String]()
-
-  override def startup(args: Array[String]){
-    strList = strList ++ args
-    super.startup(args)
+/**
+* Appends values to randomly selected keys in the map
+* and highlights updated key in the list for some
+* amount of time
+* */
+class UpdaterThread extends Thread {
+  override def run() = {
+    (1 to 100).foreach{_=>update}
+    Thread.sleep(1000)
+  }
+  def randomKey(i:Iterable[String]) = {
+    val s = i.toSeq
+    s(Random.nextInt(s.length))
+  }
+  def update = {
+    var key = ""
+    synchronized {
+      key = randomKey(App.strMap.keys)
+      App.strMap = App.strMap.updated(key, Random.nextString(20))
+    }
+    /* Update gui in Swing's event dispatch thread*/
+    Swing.onEDT {
+      App.ilv.listData = App.strMap.toSeq; 
+      App.ilv.selectIndices(App.ilv.listData.indexOf(key))
+      App.ilv.repaint()
+      App.update(App.label, key)
+    }
   }
 
+}
+/**
+*
+* Thread that puts generated strings to map in App
+* */
+class GeneratorThread extends Thread {
+  override def run() = {
+    (1 to 100).foreach(_=>update)
+  }
+  def update = {
+    synchronized {
+      val pair = (Random.nextString(10), Random.nextString(200))
+      App.strMap +=  pair
+    }
+    /* Update gui in Swing's event dispatch thread*/
+    Swing.onEDT {
+      App.ilv.listData = App.strMap.toSeq; 
+      App.ilv.selectIndices(App.ilv.listData.size - 1)
+      App.ilv.repaint()
+      App.update(App.label)
+    }
+    Thread.sleep(3000)
+  }
+
+}
+
+object App extends SimpleSwingApplication {
+  override def startup(args : Array[String]) {
+    super.startup(args)
+    new GeneratorThread().start()
+    new UpdaterThread().start()
+  }
+  val defsize = (400, 400)
+  var strMap = TreeMap[String, String]() +
+   ("ala" -> "ma kota",
+    "ala2" -> "aslds as         sfsdfss",
+    "ala3" -> "xxx"
+  )
+
+  val area = new TextArea(20, 50) {
+    lineWrap = true
+    editable = false
+  }
+  val ilv = new ListView(strMap.toSeq){
+    requestFocus()
+    renderer = ListView.Renderer(_._1)
+  }
+  var lastdata = ""
+  def update(label:Label, data:String = "") = {
+    lastdata = if (data == "") lastdata else data
+    label.text = 
+      "<html><font color=#672638 size=24px> Lista " + 
+      ilv.listData.length + 
+      " napisów. <br/>"+
+      lastdata
+      " </font>  </html>"
+  }
+    val label = new Label {
+      minimumSize = new Dimension(0, 300)
+      update(this)
+    }
   def top = new MainFrame {
     title = "Simple frame"
-    preferredSize = new Dimension(800, 600)
-    var slowo = "Lista napisów"
-    contents = new BorderPanel(){
-      val label = new Label {
-        text = "<html><font color=#672638 size=24px>" + slowo + "</font>  </html>"
-        minimumSize = new Dimension(0, 300)
-      }
+    preferredSize = new Dimension(defsize._1, defsize._2)
+    contents  = new BorderPanel(){
       val dataPanel = new BorderPanel() {
-        val ilv = new ListView(strList){
-        }
         val lv = new ScrollPane(ilv)
-        val area = new TextArea(20, 50) {
-          lineWrap = true
-          editable = false
-        }
         layout(lv) = BorderPanel.Position.North
         layout(area) = BorderPanel.Position.Center
         contents ++ Seq(lv, area)
         listenTo(ilv.mouse.clicks, ilv.selection)
         reactions += {
-          case e:MouseClicked => {
-              println("list view clicked")
-          }
           case e:ListSelectionChanged[String] => {
-            println("list view sel changed")
+            val selhead = ilv.selection.items
+            synchronized{
+              selhead.headOption map {s:(String, String) => 
+                area.text = strMap.getOrElse(s._1, "dasdas")}
+            }
           }
         }
       }
@@ -45,6 +119,7 @@ object App extends SimpleSwingApplication {
       layout(label) =  BorderPanel.Position.North
       layout(dataPanel) = BorderPanel.Position.Center
       contents ++ Seq(label, dataPanel)
+      border = Swing.EmptyBorder(5, 5, 5, 5)
     }
   }
 }
