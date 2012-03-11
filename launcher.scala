9d6449a86a2e64af9caec9f00937c37afa3dda28
Kamila Chyla <kamila.chyla@gmail.com>
2012-03-11 07:45:08 +0100

    Added AppConfig, refactored.

--- launcher.scala
+++ launcher.scala
@@ -7,46 +7,53 @@ import java.lang.Thread
 import scala.util.Random
 
 
-object App extends SimpleSwingApplication {
+class AppConfig {
   val defsize = (600, 400)
-  val emptyBorder = CompoundBorder(EtchedBorder(Lowered), EmptyBorder(5, 5, 5, 5))
+  val title = "Shell launcher"
+  val border = CompoundBorder(EtchedBorder(Lowered), EmptyBorder(5, 5, 5, 5)) 
+  val labelBgCol = new Color(200, 200, 198)
+}
 
-  def label = new TextField {
-    background = new Color(200, 250, 198)
-    columns = 20
+object App extends SimpleSwingApplication {
+  val cfg = new AppConfig()
+  def top = new MyFrame(cfg)
+}
+
+class MyFrame(cfg : AppConfig) extends MainFrame {
+  val label = new TextField {
+    background = cfg.labelBgCol
   }
 
-  val area = new TextArea(20, 50) {
+  val area = new TextArea() {
     lineWrap = true
     editable = false
   }
+  
+  
 
+  title = cfg.title
+  preferredSize = cfg.defsize
+  
+  contents  = bordered(new BorderPanel(){
+    val areaScroll = new ScrollPane(area)
+    val commandField = bordered(label)
+    listenTo(commandField)
+    reactions += {
+      case EditDone(commandField) => {
+        area.append(exec(commandField.text) + "\n")
+        commandField.selectAll
 
-  def bordered[T <:Component] (c: T): T = {
-    c.border = emptyBorder
-    return c
-  }
-
-  def top = new MainFrame {
-    title = "Simple frame"
-    preferredSize = new Dimension(defsize._1, defsize._2)
-    contents  = bordered(new BorderPanel(){
-      val areaScroll = new ScrollPane(bordered(area))
-      val commandField = bordered(label)
-      listenTo(commandField)
-      reactions += {
-        case EditDone(commandField) => {
-          area.append(commandField.text + "\n")
-          commandField.selectAll
-
-        }
       }
-      
-      layout(commandField) =  BorderPanel.Position.North
-      layout(areaScroll) = BorderPanel.Position.Center
-      contents ++ Seq(areaScroll, commandField)
-    })
+    }
     
-
+    layout(commandField) =  BorderPanel.Position.North
+    layout(areaScroll) = BorderPanel.Position.Center
+    contents ++ Seq(areaScroll, commandField)
+  })
+  
+  def exec(s:String) = 
+  def bordered[T <:Component] (c: T): T = {
+    c.border = cfg.border
+    return c
   }
 }
